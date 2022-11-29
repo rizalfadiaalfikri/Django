91ec4352fb519820c60dd72f68001b301528f2c1
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-11-29 20:50:12 +0700

    Django Templates Basics

--- Django/views.py
+++ Django/views.py
@@ -3,7 +3,8 @@
 """
 
 from django.http import HttpResponse
-
+from django.template.loader import render_to_string, get_template
+from django.shortcuts import render
 
 def home_view(request):
     """
@@ -12,8 +13,17 @@ def home_view(request):
     """
     name = "Rizal Fadia Al Fikri"
 
-    HTML_STRING = f"""
-        <h1>Hello {name}</h1>
-    """
+    context = {
+        "title" : "this is title",
+        "id" : "2"
+    }
+    
+    # Django Templates
+    # f = open("my-template.html",'r')
+    # string = f.read()
+    tmpl = get_template("home-view.html")
+    tmpl_string = tmpl.render()
+    HTML_STRING = render_to_string("home-view.html")
+    # HTML_STRING = string.format(**context)
 
     return HttpResponse(HTML_STRING)
