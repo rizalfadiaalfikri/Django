d71a4c11c6ec36f2143e59a147f56d1c15df9130
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-11-30 07:16:33 +0700

    Listing Data in Views & Templates

--- Django/views.py
+++ Django/views.py
@@ -12,18 +12,20 @@ def home_view(request):
         Return HTML as a response (We pick to return the response)
     """
     name = "Rizal Fadia Al Fikri"
-
+    my_list = [1,2,3,4,5,6]
+    
     context = {
+        "my_list": my_list,
         "title" : "this is title",
         "id" : "2"
     }
-    
+
     # Django Templates
     # f = open("my-template.html",'r')
     # string = f.read()
     tmpl = get_template("home-view.html")
     tmpl_string = tmpl.render()
-    HTML_STRING = render_to_string("home-view.html")
+    HTML_STRING = render_to_string("home-view.html", context=context)
     # HTML_STRING = string.format(**context)
 
     return HttpResponse(HTML_STRING)
