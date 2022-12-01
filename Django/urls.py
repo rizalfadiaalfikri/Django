ece2302daef8b32d47307ed8754cd49014a64544
Rizal Fadia Al Fikri <Rizalfadiaalfikri859@gmail.com>
2022-12-01 12:40:35 +0700

    add URL dan Views

--- Django/urls.py
+++ Django/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
-from .views import home_view
+from django.urls import path, re_path, include
 
 
 urlpatterns = [
-    path('', home_view),
-    path('article/<int:id>/', home_view),
+    # path('', home_view),
+    # path('article/<int:id>/', home_view),
     #url(r'article/(?P<id>\d+)/$', home_view),
     path('admin/', admin.site.urls),
+    path('', include('meetups.urls'))
 ]
--- Django/views.py
+++ Django/views.py
@@ -6,29 +6,5 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string, get_template
 from django.shortcuts import render
 
-def article_home_view(request):
-    return HttpResponse(request)
-
-def home_view(request, *args, **kwargs):
-    """
-        Take in a request (Django sends request)
-        Return HTML as a response (We pick to return the response)
-    """
-    name = "Rizal Fadia Al Fikri"
-    my_list = [1,2,3,4,5,6]
-    
-    context = {
-        "my_list": my_list,
-        "title" : "this is title",
-        "id" : "2"
-    }
-
-    # Django Templates
-    # f = open("my-template.html",'r')
-    # string = f.read()
-    tmpl = get_template("home-view.html")
-    tmpl_string = tmpl.render()
-    HTML_STRING = render_to_string("home-view.html", context=context)
-    # HTML_STRING = string.format(**context)
-
-    return HttpResponse(HTML_STRING)
+def index(request):
+    return HttpResponse('Hello World')
